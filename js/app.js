41fdef1b9741820253a211680fe3036febb95fbf
LeStitcheurF0u <mcfly@mywebsoluce.fr>
2023-03-10 17:14:14 +0100

    update au 10/03

--- js/app.js
+++ js/app.js
@@ -1,23 +1,9 @@
-//Social
-const socialBtn = document.querySelector('#social');
-const socialWindow = document.querySelector('.socialMedia');
-const close = document.querySelector('#close');
-const list = document.querySelectorAll('.sci li');
+//Déclaration de variables
+const socialBtn = document.querySelector('#social')
+const socialWindow = document.querySelector('.socialMedia')
+const close = document.querySelector('#close')
+const list = document.querySelectorAll('.sci li')
 
 socialBtn.addEventListener('click', () => {
     socialWindow.classList.add('show');
-})
-close.addEventListener('click', () => {
-    socialWindow.classList.remove('show');
-})
-
-list.forEach(elements => {
-    elements.addEventListener('mouseenter', function (event) {
-        let color = event.target.getAttribute('data-color');
-        socialWindow.style.backgroundColor = color;
-    });
-    elements.addEventListener('mouseleave', function (event) {
-        let color = event.target.getAttribute('data-color');
-        socialWindow.style.backgroundColor = "#292929";
-    });
 })
